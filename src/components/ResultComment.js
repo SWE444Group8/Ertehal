d9ac6fc49c0d504cd360d657046bb87ea39eb946
shahd alqudairi <70724057+shahdnq@users.noreply.github.com>
2020-11-08 23:43:46 +0300

    comment
    Co-Authored-By: judealkharashii <70727988+judealkharashii@users.noreply.github.com>

--- src/components/ResultComment.js
+++ src/components/ResultComment.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: "grey",
     marginLeft: 10,
-    marginTop: 18,
+    marginTop: 17,
     fontFamily: "Futura-Medium",
   },
   description: {
